12bf84fbda3473834f8d6a2ec231a7b1ec1167bd
VikashSingh0507 <vikash3378singh@gmail.com>
2025-02-18 14:26:05 +0530

    my sample c program

---
+++ sample.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+ int a,b;
+ printf("enter the value for a: ");
+ scanf("%d",&a);
+ printf("enter the value for b:");
+ scanf("%d",&b);
+ printf("the sum of two number is ",a+b);
+ return 0;
+}
